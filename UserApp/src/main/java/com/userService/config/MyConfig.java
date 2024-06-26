bf8a47dfa6b5efda2f9d39a1da6b074dba9da137
Jaymala <jayusutar06@gmail.com>
2024-06-26 12:45:14 +0530

    26-6-2024

--- UserApp/src/main/java/com/userService/config/MyConfig.java
+++ UserApp/src/main/java/com/userService/config/MyConfig.java
@@ -14,4 +14,17 @@ public class MyConfig {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	
+	
+	/*By using @LoadBalanced with RestTemplate, you enable client-side load balancing in your Spring Boot application. 
+	  This allows your application to distribute requests across multiple instances of a service, which can improve 
+	  availability and scalability. The key steps are to add the necessary dependencies, configure your application 
+	  as a Eureka client, and define a load-balanced RestTemplate bean.
+	  
+	  The @LoadBalanced annotation essentially wraps the RestTemplate with a LoadBalancerInterceptor.
+	  
+	  
+	*/
+	
 }
